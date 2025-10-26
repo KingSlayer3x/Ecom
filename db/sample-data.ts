eab8f359f82813e5d4eaba30c6a58d9be306d95d
KingSlayer3X <alito321a@gmail.com>
2025-10-26 23:59:37 +0300

    setting connection

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,19 @@
+import { hashSync } from "bcrypt-ts-edge";
+
 const sampleData = {
+  users: [{
+    name: 'john',
+    email: 'admin@example.com',
+    password: hashSync('123456', 10),
+    role: 'admin'
+  },
+  {
+    name: 'Ali',
+    email: 'user@example.com',
+    password: hashSync('123456', 10),
+    role: 'user',
+  }
+],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
