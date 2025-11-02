089b4114a219aa74664116c8b20516401f94d3c2
KingSlayer3X <alito321a@gmail.com>
2025-11-02 22:44:10 +0200

    setting sign_up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,30 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Link } from "lucide-react";
 import { Metadata } from "next";
-
+import Image from "next/image";
 export const metadata: Metadata = {
-    title: Sign In
-}
+    title: 'Sign In',
+};
 
 const SignInPage = () => {
-    return ( <div className="w-full max=w-md">Sign In</div> );
+    return ( <div className="w-full max-w-md mx-auto">
+    <Card>
+        <CardHeader className="space-y-4">
+            <Link href="/" className="flex-center">
+                <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true} />
+            </Link>
+            <CardTitle className="text-center">Sign In</CardTitle>
+            <CardDescription className="text-center">
+                Sign in to your account
+            </CardDescription>
+        </CardHeader>
+        <CardContent className="space-4">
+            {/* */}
+        </CardContent>
+    </Card>
+
+</div> );
 }
- 
+
 export default SignInPage;
