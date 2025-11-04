f5cc936a8c835385f2909657d17516b882cd3275
KingSlayer3X <alito321a@gmail.com>
2025-11-04 20:23:00 +0200

    setting sign_up form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,9 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { APP_NAME } from "@/lib/constants";
-import { Link } from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
+import CredentialsSigninForm from "./credentials-signin-form";
+import Link from "next/link";
 export const metadata: Metadata = {
     title: 'Sign In',
 };
@@ -12,7 +13,7 @@ const SignInPage = () => {
     <Card>
         <CardHeader className="space-y-4">
             <Link href="/" className="flex-center">
-                <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true} />
+                <Image src='/images/logo.svg' width={50} height={50} alt={`${APP_NAME} logo`} priority={true} layout="intrinsic" />
             </Link>
             <CardTitle className="text-center">Sign In</CardTitle>
             <CardDescription className="text-center">
@@ -20,7 +21,7 @@ const SignInPage = () => {
             </CardDescription>
         </CardHeader>
         <CardContent className="space-4">
-            {/* */}
+            <CredentialsSigninForm />
         </CardContent>
     </Card>
 
