ada0de1f930671ec3dd1b20f911f2fcde2f7ed4c
KingSlayer3X <alito321a@gmail.com>
2025-11-04 21:26:29 +0200

    setting sign_up form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,11 +4,23 @@ import { Metadata } from "next";
 import Image from "next/image";
 import CredentialsSigninForm from "./credentials-signin-form";
 import Link from "next/link";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 export const metadata: Metadata = {
     title: 'Sign In',
 };
 
-const SignInPage = () => {
+const SignInPage = async (props: {
+    searchParams: Promise<{
+        callbackUrl: string
+    }>
+}) => {
+        const {callbackUrl} = await props.searchParams
+        const session = await auth();
+        if(session) {
+            return redirect(callbackUrl || '/');
+        }
     return ( <div className="w-full max-w-md mx-auto">
     <Card>
         <CardHeader className="space-y-4">
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -15,15 +15,15 @@ export async function signInWithCredentials(prevState: unknown,
                 password: formData.get('password'),
             });
             await signIn('credentials', user);
-            return { success: true, message: 'Signed in successfully' };
-        } catch (error) {
+
+            return { success: 'true', message: 'Signed in successfully' };
+          } catch (error) {
             if(isRedirectError(error)){
                 throw error;
             }
-            return { success: false, messgae: 'Invalid email or password' };
+            return { success: false, message: 'Invalid email or password' };
         }
     }
-
     // sign user out
     export async function signOutUser(){
         await signOut();
